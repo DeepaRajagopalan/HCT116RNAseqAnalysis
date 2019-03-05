c15f6a466080bd12ffb38b30861979b10b72adbd
DeepaRajagopalan <44390495+DeepaRajagopalan@users.noreply.github.com>
2019-03-05 13:53:17 +0800

    Update CodeforR.R

--- CodeforR.R
+++ CodeforR.R
@@ -1,13 +1,13 @@
 Create genome directory first-
 To map, follow this
 
-/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/newgenome/rawdata/HCT116_siC_1.fq.gz /home/deepa/newgenome/rawdata/HCT116_siC_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/newgenome/star_output/siC/siC1 --outSAMtype BAM SortedByCoordinate 
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/Hep3B_Analysis/DMSO_1.fq.gz /home/deepa/Hep3B_Analysis/DMSO_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/Hep3B_Analysis/star_output/DMSO/D1 --outSAMtype BAM SortedByCoordinate 
 
-/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/newgenome/rawdata/HCT_siControl_HWN2YCCXX_L5_1.fq.gz /home/deepa/newgenome/rawdata/HCT_siControl_HWN2YCCXX_L5_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/newgenome/star_output/siC/siC2 --outSAMtype BAM SortedByCoordinate 
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/Hep3B_Analysis/Hep3BDMSO_HWN2YCCXX_L5_1.clean.fq.gz  /home/deepa/Hep3B_Analysis/Hep3BDMSO_HWN2YCCXX_L5_2.clean.fq.gz  --readFilesCommand zcat --outFileNamePrefix /home/deepa/Hep3B_Analysis/star_output/DMSO/D2 --outSAMtype BAM SortedByCoordinate 
 
-/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/newgenome/rawdata/HCT116_siK_1.fq.gz /home/deepa/newgenome/rawdata/HCT116_siK_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/newgenome/star_output/siT/siT1 --outSAMtype BAM SortedByCoordinate
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/Hep3B_Analysis/JQ1_1.fq.gz /home/deepa/Hep3B_Analysis/JQ1_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/Hep3B_Analysis/star_output/JQ1/J1 --outSAMtype BAM SortedByCoordinate
 
-/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/newgenome/rawdata/HCT_siK_HVNYLCCXX_L2_1.fq.gz /home/deepa/newgenome/rawdata/HCT_siK_HVNYLCCXX_L2_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/newgenome/star_output/siT/siT2 --outSAMtype BAM SortedByCoordinate 
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 18 --genomeDir /home/deepa/newgenome/data/GRCh38/star_indices_overhang100 --readFilesIn /home/deepa/Hep3B_Analysis/Hep3BJQ1_HWN2YCCXX_L5_1.clean.fq.gz  /home/deepa/Hep3B_Analysis/Hep3BJQ1_HWN2YCCXX_L5_2.clean.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/deepa/Hep3B_Analysis/star_output/JQ1/J2 --outSAMtype BAM SortedByCoordinate 
 
 
 # NOT Necessary to use
